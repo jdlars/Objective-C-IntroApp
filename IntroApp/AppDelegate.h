20f5deecfffb691ae6445725de2f8e1cfa8679f1
Larson <jlrh3@kc-sce463lab-14.kc.umkc.edu>
2015-09-10 14:34:45 -0500

    Initial Commit

---
+++ IntroApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  IntroApp
+//
+//  Created by Larson, Jordan (UMKC-Student) on 9/10/15.
+//  Copyright (c) 2015 Larson, Jordan (UMKC-Student). All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ IntroApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  IntroApp
+//
+//  Created by Larson, Jordan (UMKC-Student) on 9/10/15.
+//  Copyright (c) 2015 Larson, Jordan (UMKC-Student). All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
